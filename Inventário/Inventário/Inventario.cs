0a1c2ed7c60fb1b7d4a16927a0f623c993e89cbf
Luis-Henrique-Santana <lhs3000t@gmail.com>
2024-08-27 15:50:13 -0300

    criação de sistema de inventário

---
+++ Inventário/Inventário/Inventario.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventário
+{
+    class Inventario
+    {
+        //atri
+        
+        //propriedades
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+        public double Valor { get; set; }
+
+        //metodos
+
+        //depositar ------------------
+        public void Depositar(int Valor)
+        {    
+            if(Valor > 0)
+            {
+                Quantidade = Quantidade + Valor;
+                Console.WriteLine("Valor " + Valor + " Quantidade " + Quantidade);
+            }
+            else
+            {
+                Console.WriteLine("Adicionar negativo não funfa");
+               
+            }
+
+            
+        }
+        //Sacar ------------
+        public void Sacar(int Valor)
+        {
+            
+            if (Valor <= Quantidade)
+            {
+                Quantidade = Quantidade - Valor;
+                
+            }
+            else
+            {
+                Console.WriteLine("quantidade menor que retirada");
+                
+            }
+            Console.WriteLine("Valor " + Valor + " Quantidade " + Quantidade);
+        }
+
+        //exibir -----------
+        public void  Exibir()
+        {
+            Console.Write(" ID: " + Id);
+            Console.Write(" Nome: " + Nome);
+            Console.Write(" Quantidade: " + Quantidade);
+            
+        }
+
+
+        //construtor
+        public Inventario(int id, string nome, double preco, int valor )
+        {
+            Id = id;
+            Nome = nome;
+            Valor = valor;
+        }
+
+
+    }
+}
---
+++ Inventário/Inventário/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventário
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("digite 1 para entrar e 0 para sair");
+                int opcao;
+                if (!int.TryParse(Console.ReadLine(), out opcao))
+                {
+                    Console.WriteLine("opção invalida");
+                    continue;
+
+                }
+                if (opcao == 0)
+                {
+                    Console.WriteLine("ok 😎👍");
+                    break;
+
+                }
+                else if (opcao == 1)
+                {
+                    Console.WriteLine("ID: ");
+                    int id = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Nome: ");
+                    string nome = Console.ReadLine();
+
+                    Console.WriteLine("Preco: ");
+                    double preco = double.Parse(Console.ReadLine());
+
+                    int valor = 0;
+                    Inventario obj1 = new Inventario(id, nome, preco, valor);
+
+                    // seleciona opção
+
+                    while (true)
+                    {
+                        Console.WriteLine("digite 1 para Adicionar, 2 para Retirar, 3 para exibir e 0 para sair");
+                        int opcao1;
+                        if (!int.TryParse(Console.ReadLine(), out opcao1))
+                        {
+                            Console.WriteLine("opção invalida");
+                            continue;
+
+                        }
+                        if (opcao1 == 0)
+                        {
+                            Console.WriteLine("ok 😎👍");
+                            break;
+
+                        }
+
+                        else if (opcao1 == 2)
+                        {
+                            Console.WriteLine("Retirar: ");
+                            valor = int.Parse(Console.ReadLine());
+                            obj1.Sacar(valor);
+                        }
+                        else if (opcao1 == 1)
+                        {
+                            Console.WriteLine("Adicionar: ");
+                            valor = int.Parse(Console.ReadLine());
+                            obj1.Depositar(valor);
+                        }
+                        else if (opcao1 == 3)
+                        {
+                            obj1.Exibir();
+                            /*
+                            Console.WriteLine($"ID = {obj1.Id}");
+                            Console.WriteLine($"Nome = {obj1.Nome}");
+                            Console.WriteLine($"Quantidade = {obj1.Quantidade}");
+                            Console.WriteLine($"Preco = {obj1.Preco}");*/
+                        }
+
+
+
+
+                    }
+
+
+
+                }
+
+
+
+            }
+        }
+    }
+}
